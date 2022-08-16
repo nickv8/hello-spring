c4f82381e692237f35246fd18c00894932b5fb02
Nick Villone <n.villone8@gmail.com>
2022-08-15 22:02:34 -0400

    completed mapping with Spring

---
+++ main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,24 @@
+package org.launchcode.hellospring.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    //Handles request at /hello
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodBye() {
+        return "GoodBye, Spring!";
+    }
+
+}
