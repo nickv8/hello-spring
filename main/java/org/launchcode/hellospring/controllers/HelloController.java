9f60593ad94276791f1b4116024aa8f0a5ca55c4
Nick Villone <n.villone8@gmail.com>
2022-08-15 22:44:36 -0400

    handle requests with params)
    clear
    
    git
    git status
    clear

--- main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,23 +2,45 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
     //Handles request at /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye")
     @ResponseBody
     public String goodBye() {
         return "GoodBye, Spring!";
     }
+
+    @RequestMapping(value="hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST })
+    @ResponseBody
+        public String helloGoodbye(){
+            return "Hello and Goodbye together";
+        }
+
+
+        //Handles request of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+        public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+        }
+
+
+        //Handles requests of the form /hello/LaunchCode
+//    @GetMapping("hello/{name}")
+//    @ResponseBody
+//        public String helloWithPathParam(@PathVariable String name){
+//        return "Hello, " + name + "!";
+//    }
+
 
 }
