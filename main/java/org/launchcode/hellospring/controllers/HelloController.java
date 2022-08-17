dfb88acdda616ac17102bad43c4e125c33b5c400
Nick Villone <n.villone8@gmail.com>
2022-08-16 20:07:17 -0400

    added Class controllers and DRY code

--- main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,42 +5,58 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
-    //Handles request at /hello
+    //Handles request at path /hello
 //    @GetMapping("hello")
 //    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
 
+    //now is found at localhost:8080/hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodBye() {
         return "GoodBye, Spring!";
     }
 
-    @RequestMapping(value="hellogoodbye", method = {RequestMethod.GET, RequestMethod.POST })
-    @ResponseBody
-        public String helloGoodbye(){
-            return "Hello and Goodbye together";
-        }
-
-
-        //Handles request of the form /hello?name=LaunchCode
-    @GetMapping("hello")
-    @ResponseBody
+    //lives at hello/hello
+    //handles request of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST })
         public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+            return "Hello, " + name + "!";
         }
+
+
+        //above does same as this
+        //Handles request of the form /hello?name=LaunchCode
+//    @GetMapping("")
+//
+//        public String helloWithQueryParam(@RequestParam String name){
+//        return "Hello, " + name + "!";
+//        }
 
 
         //Handles requests of the form /hello/LaunchCode
-//    @GetMapping("hello/{name}")
-//    @ResponseBody
-//        public String helloWithPathParam(@PathVariable String name){
-//        return "Hello, " + name + "!";
-//    }
+    @GetMapping("/{name}")
+        public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    //lives at /hello/form
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action = 'hello' method = 'post'/>" + //submit a request to /hello
+                "<input type = 'text' value = 'Great Me!'/>" +
+                "</form>" +
+                "</body>" +
+                "/html>";
+
+}
 
 
 }
